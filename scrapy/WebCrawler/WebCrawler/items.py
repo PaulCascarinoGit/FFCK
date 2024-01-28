8ada1030124e185fb9b1bf28be56f6aa502227b4
mathisqc <150790032+mathisqc@users.noreply.github.com>
2024-01-28 19:43:28 +0100

    Add files via upload

---
+++ scrapy/WebCrawler/WebCrawler/items.py
@@ -0,0 +1,24 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class CanoeKayakScrapItem(scrapy.Item):
+    
+    Rank = scrapy.Field()
+    Scratch = scrapy.Field()
+    Name = scrapy.Field()
+    Club = scrapy.Field()
+    Boat = scrapy.Field()
+    Category = scrapy.Field()
+    Year = scrapy.Field()
+    Division = scrapy.Field()
+    Points_Club = scrapy.Field()
+    Points = scrapy.Field()
+    Num_Courses = scrapy.Field()
+    Courses_Nat = scrapy.Field()
+
+    pass
